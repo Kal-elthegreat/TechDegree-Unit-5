078bc33029f93bcbc8c5b8f6ee3ed220654981d6
Kiel Lewis <kiellewis@Kiels-MacBook-Pro.local>
2019-01-05 18:24:24 -0500

    refactored code using jQuery

--- Public API Request/Starter Files/js/app.js
+++ Public API Request/Starter Files/js/app.js
@@ -1,25 +1,18 @@
-var directory = new XMLHttpRequest();
-directory.onreadystatechange = function () {
-    if(directory.readyState === 4){
-        let employees = JSON.parse(directory.responseText);
-        let employeeList = employees.results;
-        
-        for(let i = 0; i < employeeList.length; i++){
-        dirHTML = '<div class="card">'
-        dirHTML += '<div class="card-img-container">'
-        dirHTML += '<img class="card-img" src="' + employeeList[i].picture.large +'" alt= "bird">'
-        dirHTML += '</div>' // img-container
-        dirHTML += '<div class="card-info-container">'
-        dirHTML += '<h3 id="name" class="card-name cap">' + employeeList[i].name.first + ' ' + employeeList[i].name.last + '</h3>'
-        dirHTML += '<p class="card-text">' + employeeList[i].email + '</p>'
-        dirHTML += '<p class="card-text cap">' + employeeList[i].location.city + ', ' + employeeList[i].location.state + '</p>'
-        dirHTML += '</div>' // info-container
-        dirHTML += '</div>' //  card
-        document.getElementById('gallery').innerHTML += dirHTML;
-        }
-
-    }
-};
-
-directory.open('GET', 'https://randomuser.me/api/?results=12');
-directory.send();
+$(document).ready(function(){
+    var url = 'https://randomuser.me/api/?results=12'
+    $.getJSON(url, function(response){
+        $.each(response.results, function(index,employee){
+            dirHTML = '<div class="card">'
+            dirHTML += '<div class="card-img-container">'
+            dirHTML += '<img class="card-img" src="' + employee.picture.large +'">'
+            dirHTML += '</div>' // img-container
+            dirHTML += '<div class="card-info-container">'
+            dirHTML += '<h3 id="name" class="card-name cap">' + employee.name.first + ' ' + employee.name.last + '</h3>'
+            dirHTML += '<p class="card-text">' + employee.email + '</p>'
+            dirHTML += '<p class="card-text cap">' + employee.location.city + ', ' + employee.location.state + '</p>'
+            dirHTML += '</div>' // info-container
+            dirHTML += '</div>' //  card
+            document.getElementById('gallery').innerHTML += dirHTML;
+        }) // end each
+    }) // getJSON
+});// end ready
